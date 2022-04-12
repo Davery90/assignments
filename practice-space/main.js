1ad7d54211592bd442bace5c005a95b6dea89b8a
David Avery <davery736@gmail.com>
2022-04-12 13:03:31 -0400

    bugs-on-a-plane

--- practice-space/main.js
+++ practice-space/main.js
@@ -1,6 +1,23 @@
-var result = document.querySelectorAll(".one")
-console.log(result)
+var numbersArr = [23, 64, 20, 33, 40, 100, 30, 50, 2]
+var petsArr = ["cat", "dog", "bird", "snake", "rabbit"]
+var users = [
+    {
+        name: "joe",
+        age: 30,
+    },
+    {
+        name: "steve",
+        age: 23
+    },
+    {
+        name: "sarah",
+        age: 39
+    },
+    {
+        name: "lisa",
+        age: 27
+    }
+]
 
-let breh = document.getElementById("button"){
-    
-}
+for(var i = 0; i < 18; i++)
+numbersArr[i]
